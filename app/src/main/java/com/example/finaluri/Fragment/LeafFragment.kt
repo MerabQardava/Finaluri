b288cf309f9156385d10932b090fceaf59659513
User <merab.kardava.1@btu.edu.ge>
2021-02-06 16:13:48 +0400

    BMI

--- app/src/main/java/com/example/finaluri/Fragment/LeafFragment.kt
+++ app/src/main/java/com/example/finaluri/Fragment/LeafFragment.kt
@@ -2,10 +2,7 @@ package com.example.finaluri.Fragment
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.finaluri.R
 
@@ -15,6 +12,7 @@ class LeafFragment:Fragment(R.layout.fragment_leaf) {
     private lateinit var Text: TextView
     private lateinit var Height:EditText
     private lateinit var Weight:EditText
+    private lateinit var Image:ImageView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -23,6 +21,7 @@ class LeafFragment:Fragment(R.layout.fragment_leaf) {
         Text = view.findViewById(R.id.textView)
         Height = view.findViewById(R.id.height)
         Weight = view.findViewById(R.id.weight)
+        Image = view.findViewById(R.id.imageView2)
 
 
 
@@ -38,14 +37,21 @@ class LeafFragment:Fragment(R.layout.fragment_leaf) {
                 val rounded = String.format("%.1f", result)
                 Text.text=rounded
 
-                if (rounded.toDouble()<18.5)
-                    Text.text=rounded + " - ძვალი და ტყავი"
-                else if (rounded.toDouble()>=18.5 && rounded.toDouble()<=24.9)
-                    Text.text=rounded + " - ნორმალური"
-                else if (rounded.toDouble()>=25 && rounded.toDouble()<=29.9)
+                if (rounded.toDouble()<18.5) {
+                    Image.setImageResource(R.drawable.dzvali_da_tyavi)
+                    Text.text = rounded + " - ძვალი და ტყავი"
+                }
+                else if (rounded.toDouble()>=18.5 && rounded.toDouble()<=24.9) {
+                    Text.text = rounded + " - ნორმალური"
+                    Image.setImageResource(R.drawable.bmikalkulatori)
+                }
+                else if (rounded.toDouble()>=25 && rounded.toDouble()<=29.9){
                     Text.text=rounded + " - ცოტა დაიკელი"
+                    Image.setImageResource(R.drawable.cota_gaxdi)
+                    }
                 else {
                     Text.text=rounded + " - ჯვარი გწერია"
+                    Image.setImageResource(R.drawable.gmertma_dagifaros)
                 }
 
 
--- app/src/main/java/com/example/finaluri/PersonActivity.kt
+++ app/src/main/java/com/example/finaluri/PersonActivity.kt
@@ -10,7 +10,7 @@ import com.example.finaluri.Fragment.MealFragment
 import kotlinx.android.synthetic.main.activity_person.*
 
 class PersonActivity:AppCompatActivity() {
-    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_person)
